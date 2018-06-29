d0aebface034b5f054c0a43497a9e93a5112b8e8
ROOT <ROOT@ROOT-PC>
2018-06-29 19:47:42 -0300

    Adicionar arquivos de projeto.

---
+++ SADTRESULT/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SADTRESULT
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer1;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer(new TimerCallback(timer1_Tick), null, 15000, 60000);
+        }
+
+        protected override void OnStop()
+        {
+            StreamWriter vWriter = new StreamWriter(@"c:\testeServico.txt", true);
+
+            vWriter.WriteLine("Servico Parado: " + DateTime.Now.ToString());
+            vWriter.Flush();
+            vWriter.Close();
+        }
+        private void timer1_Tick(object sender)
+        {
+            StreamWriter vWriter = new StreamWriter(@"c:\testeServico.txt", true);
+            vWriter.WriteLine("Servico Rodando: " + DateTime.Now.ToString());
+            vWriter.Flush();
+            vWriter.Close();
+        }
+    }
+}
+
